3ce82bd07c36b58aad8b6ca7a12bcf847a4a4848
zhaoyz <954241552@qq.com>
2021-06-15 18:54:29 +0800

    修改发现页面布局展示

--- front_end/src/components/FindMore.vue
+++ front_end/src/components/FindMore.vue
@@ -1,35 +1,30 @@
 <template>
   <el-row>
-    <el-col :span="7">
+    <el-col :span="8">
       <el-card shadow="always">
-        <el-link type="primary" :underline="false" href="https://tool.oschina.net/" target="_blank">OSCHINA 工具列表</el-link>
+        <el-link type="primary" :underline="false" href="http://www.shouce.ren/tool/yanse" target="_blank">颜色选择</el-link>
       </el-card>
     </el-col>
-    <el-col :span="7">
+    <el-col :span="8">
+      <el-card shadow="always">
+        <el-link type="primary" :underline="false" href="https://codepan.egoist.sh/" target="_blank">前端代码编辑器</el-link>
+      </el-card>
+    </el-col>
+    <el-col :span="8">
       <el-card shadow="always">
         <el-link type="primary" :underline="false" href="num-to-chinese" target="_blank">数字金额转中文金额</el-link>
       </el-card>
     </el-col>
-    <el-col :span="7">
+    <el-col :span="8">
+      <el-card shadow="always">
+        <el-link type="primary" :underline="false" href="https://tool.oschina.net/" target="_blank">OSCHINA 工具列表</el-link>
+      </el-card>
+    </el-col>
+    <el-col :span="8">
       <el-card shadow="always">
         <el-link type="primary" :underline="false" href="unicode-to-chinese" target="_blank">Unicode 转 中文</el-link>
       </el-card>
     </el-col>
-    <!-- <el-col :span="7">
-      <el-card shadow="always">
-        鼠标悬浮时显示
-      </el-card>
-    </el-col>
-    <el-col :span="7">
-      <el-card shadow="always">
-        从不显示
-      </el-card>
-    </el-col>
-    <el-col :span="7">
-      <el-card shadow="always">
-        从不显示
-      </el-card>
-    </el-col> -->
   </el-row>
 </template>
 
@@ -48,15 +43,16 @@ export default defineComponent({
 
 <style scoped>
   .el-row {
-    margin-left: 100px;
     margin-top: 12px;
     text-align: center;
   }
   .el-link {
+    display: block;
     font-size: 18px;
+    text-decoration: none;
   }
   .el-col {
     margin-bottom: 10px;
-    margin-right: 24px;
+    padding: 1em 1.5em;
   }
 </style>
